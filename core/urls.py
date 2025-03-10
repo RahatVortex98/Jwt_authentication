d1598c04bc9cd608a967ce69d10c1c19d82383ac
RahatVortex98 <r072islam@gmail.com>
2025-03-10 08:31:36 -0700

    login and get notes

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import get_notes
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('get_notes/',get_notes,name='get_notes')
+  
+]
